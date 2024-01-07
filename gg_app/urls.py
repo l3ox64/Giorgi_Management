14032fd0dff6abc2d97018568380d20a925185b9
l3ox64 <l3ox0705@gmail.com>
2024-01-07 09:55:57 +0100

    register-form

--- gg_app/urls.py
+++ gg_app/urls.py
@@ -2,9 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView  # new
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path('register/', views.sign_up, name='register'),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
