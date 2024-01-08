a64ff0f0e6e32c7eb0ecb72e62481aa4e5ae9279
l3ox64 <l3ox0705@gmail.com>
2024-01-08 18:33:36 +0100

    managed-user-auth

--- gg_app/urls.py
+++ gg_app/urls.py
@@ -6,7 +6,8 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path('register/', views.sign_up, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('signup/', views.user_signup, name='signup'),
+    path('logout/', views.user_logout, name='logout'),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
--- gg_app/views.py
+++ gg_app/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth import login, authenticate, logout
-from .forms import LoginForm, RegisterForm
+from django.contrib.auth import authenticate, login, logout 
+from .forms import UserCreationForm, LoginForm
 
-def sign_up(request):
-    if request.method == 'GET':
-        form = RegisterForm()
-        return render(request, 'registration/register.html', {'form': form})    
-   
+# signup page
+def user_signup(request):
     if request.method == 'POST':
-        form = RegisterForm(request.POST) 
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
-            user.username = user.username.lower()
-            user.save()
-            messages.success(request, 'You have singed up successfully.')
-            login(request, user)
-            return redirect('posts')
-        else:
-            return render(request, 'registration/register.html', {'form': form})
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+# login page
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)    
+                return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+# logout page
+def user_logout(request):
+    logout(request)
+    return redirect('login')
     
